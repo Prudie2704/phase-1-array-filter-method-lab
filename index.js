d4ed0486ce760429f01bcf659c435dcc6cd1c12a
prudence <prudence.jerono@student.moringaschool.com>
2023-03-28 12:04:32 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution here
+function findMatching(Bobby){
+    return Bobby = Bobby
+}
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'].filter(findMatching);
+
+ drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'] 
+function filterItems(arr, query){
+    return arr.filter((el) =>
+    el.toLowerCase().includes(query.toLowerCase()));  
+}
+
+console.log(filterItems(drivers, "Sa"));
+
+beforeEach(function () {
+    drivers.length = 0;
+
+    drivers.push('Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby');
+    const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'] .filter(fuzzyMatch);
+}
+
+function matchName()    
+   drivers = [
+    {
+        name: 'Bobby',
+        hometown: 'Pittsburgh' },
+      {
+        name: 'Sammy',
+        hometown: 'New York' } ,
+      {
+        name: 'Sally',
+        hometown: 'Cleveland' },
+      {
+        name: 'Annette',
+        hometown: 'Los Angeles' },
+      {
+        name: 'Bobby',
+        hometown: 'Tampa Bay' }
+    ]; 
+  
